e070d3a331ff7631cc2f6011b7ed809b99e3ff14
michael oreilly <michaelheartbound99@hotmail.com>
2021-06-19 22:33:09 -0600

    json fix

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -7,7 +7,7 @@ function Resume() {
 				<h1 className="page-header">My Resume</h1>
 			</div>
 			<div className="bottom-spacing">
-				<a href={require("../../assets/files/Owais' Resume.pdf")} download>
+				<a href={require("../../assets/files/resume m.pdf")} download>
 					<h4>Download my Resume</h4>
 				</a>
 			</div>
